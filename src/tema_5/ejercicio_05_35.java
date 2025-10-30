cd5d94808d86a362efa0188d879bb3136bc13edc
Juanma Fernández <juanmafr2007@gmail.com>
2025-10-30 22:36:05 +0100

    ejercicio_05_35

---
+++ src/tema_5/ejercicio_05_35.java
@@ -0,0 +1,26 @@
+package tema_5;
+
+public class ejercicio_05_35 {
+    public static void main(String[] args) {
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la X: "));
+        int fila=1;
+        if (altura>=3) {
+            while (fila <= altura) {
+                int col = 1;
+                while (col <= altura) {
+                    if (col == fila || col == altura - fila + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                    col++;
+                }
+                System.out.println();
+                fila++;
+            }
+        } else {
+            System.out.println("Debe introducir una altura mayot o igual a 3.");
+            return;
+        }
+    }
+}

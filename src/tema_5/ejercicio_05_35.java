623b37ae0ad90381c972d3ccb3bddc2c9e8b91d1
Juan Manuel Fernández Rodríguez <juanmafr2007@gmail.com>
2025-11-05 23:05:26 +0100

    fix: ejercicio_05_35

--- src/tema_5/ejercicio_05_35.java
+++ src/tema_5/ejercicio_05_35.java
@@ -2,25 +2,31 @@ package tema_5;
 
 public class ejercicio_05_35 {
     public static void main(String[] args) {
-        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la X: "));
-        int fila=1;
-        if (altura%2!=0&&altura>=3) {
-            while (fila <= altura) {
-                int col = 1;
-                while (col <= altura) {
-                    if (col == fila || col == altura - fila + 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
+        try {
+            int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura de la X: "));
+            int fila = 1;
+            if (altura % 2 != 0 && altura >= 3) {
+                while (fila <= altura) {
+                    int col = 1;
+                    while (col <= altura) {
+                        if (col == fila || col == altura - fila + 1) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
+                        col++;
                     }
-                    col++;
+                    System.out.println();
+                    fila++;
                 }
-                System.out.println();
-                fila++;
+            } else {
+                System.out.println("Debe introducir una altura mayor o igual a 3.");
+                return;
             }
-        } else {
-            System.out.println("Debe introducir una altura mayor o igual a 3.");
-            return;
+        } catch (NumberFormatException e) {
+            System.out.println("Ha introducido un valor incorrecto.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado.");
         }
     }
 }

efaa57eb41da0b66bbb335a977d64c23ba50847e
Juanma Fernández <juanmafr2007@gmail.com>
2025-11-06 22:10:35 +0100

    fix: ejercicio_05_37

--- src/tema_5/ejercicio_05_37.java
+++ src/tema_5/ejercicio_05_37.java
@@ -4,6 +4,7 @@ public class ejercicio_05_37 {
     public static void main(String[] args) {
         try {
             int num = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+            System.out.printf("El %d en decimal es el ",num);
             int aux = num*10+1;
             int inv = 0;
             int digitos = 0;
@@ -16,12 +17,15 @@ public class ejercicio_05_37 {
             int aux2 = inv;
             while (aux2!=1) {
                 int numPalote = aux2%10;
+                inv = inv*10+numPalote; 
                 for (int i=1; i<=numPalote; i++) {
                     System.out.print("|");
                 }
-                if (numPalote<digitos-1) System.out.print("-");
+                digitos--;
+                if (digitos-1>0) System.out.print("-");
                 aux2/=10;
             }
+            System.out.print(" en el sistema de palotes.");
         } catch (NumberFormatException e) {
             System.out.println("Ha introducido un valor incorrecto.");
         } catch (Exception e) {

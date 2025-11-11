9d03dd9bdf80d188accdc62235c03f99ca1fa9af
Juanma Fernández <juanmafr2007@gmail.com>
2025-11-12 00:16:31 +0100

    ejercicio_05_67_TOFIX

---
+++ src/tema_5/ejercicio_05_67.java
@@ -0,0 +1,20 @@
+package tema_5;
+
+public class ejercicio_05_67 {
+    public static void main(String[] args) {
+        try {
+            int numEscalones = Integer.parseInt(System.console().readLine("Introduzca el número de escalones: "));
+            int alturaEscalones = Integer.parseInt(System.console().readLine("Introduzca la altura de cada escalón: "));
+            int fila = 1;
+            // while (fila<=numEscalones*alturaEscalones) {
+            //     for (int i=1; i<=numEscalones; i+=numEscalones) System.out.print("****");
+            //     System.out.println();
+            //     fila++;
+            // }
+        } catch (NumberFormatException e) {
+            System.out.println("Ha introducido un valor incorrecto.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado.");
+        }
+    }
+}

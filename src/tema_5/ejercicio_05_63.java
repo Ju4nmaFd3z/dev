fe73664cc9e916268ee2c44b940bb524ea478f29
Juan Manuel Fernández Rodríguez <jferrod0510@g.educaand.es>
2025-11-11 12:06:30 +0100

    ejercicio_05_63_TOFIX

---
+++ src/tema_5/ejercicio_05_63.java
@@ -0,0 +1,41 @@
+package tema_5;
+
+public class ejercicio_05_63 {
+    public static void main(String[] args) {
+        try {
+            int altura1 = Integer.parseInt(System.console().readLine("Introduzca la altura de la primera pirámide: "));
+            int altura2 = Integer.parseInt(System.console().readLine("Introduzca la altura de la segunda pirámide: "));
+            int altMax = Math.max(altura1, altura2);
+            int fila = 1;
+            while (fila<=altMax) {
+                if (altura1>altura2) {
+                    for (int i=1; i<=altura1-fila; i++) System.out.print(" ");
+                    for (int i=1; i<=2*fila-1; i++) System.out.print("*");
+                    for (int i=1; i<=altura1-fila; i++) System.out.print(" ");
+                    System.out.print(" ");
+                    if (!(fila<=altMax-altura2)) {
+                        // System.out.print("ASDASDASDA");
+                        for (int i=1; i<=altMax-fila; i++) System.out.print("8");
+                        for (int i=1; i<=2*fila-altura1; i++) System.out.print("*");
+                        // for (int i=1; i<=altura1-fila; i++) System.out.print(" ");
+                        // for (int i=1; i<=2*fila-altMax-1; i++) System.out.print("*");
+                    }
+                }
+                if (altura1==altura2) {
+                    for (int i=1; i<=altura1-fila; i++) System.out.print(" ");
+                    for (int i=1; i<=2*fila-1; i++) System.out.print("*");
+                    for (int i=1; i<=altura1-fila; i++) System.out.print(" ");
+                    System.out.print(" ");
+                    for (int i=1; i<=altura2-fila; i++) System.out.print(" ");
+                    for (int i=1; i<=2*fila-1; i++) System.out.print("*");
+                }
+                System.out.println();
+                fila++;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Ha introducido un valor incorrecto.");
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error inesperado.");
+        }
+    }
+}

d62e9666a8fa9e9bbf77b9bee1cdce5a7e4c3303
Juan Manuel Fernández Rodríguez <juanmafr2007@gmail.com>
2025-11-12 10:04:40 +0100

    ejercicio_05_67

--- src/tema_5/ejercicio_05_67.java
+++ src/tema_5/ejercicio_05_67.java
@@ -6,11 +6,13 @@ public class ejercicio_05_67 {
             int numEscalones = Integer.parseInt(System.console().readLine("Introduzca el número de escalones: "));
             int alturaEscalones = Integer.parseInt(System.console().readLine("Introduzca la altura de cada escalón: "));
             int fila = 1;
-            // while (fila<=numEscalones*alturaEscalones) {
-            //     for (int i=1; i<=numEscalones; i+=numEscalones) System.out.print("****");
-            //     System.out.println();
-            //     fila++;
-            // }
+            while (fila<=alturaEscalones*numEscalones) {
+                for (int i=1; i<= fila; i+=2) {
+                    System.out.print("****");
+                }
+                System.out.println();
+                fila++;
+            }
         } catch (NumberFormatException e) {
             System.out.println("Ha introducido un valor incorrecto.");
         } catch (Exception e) {

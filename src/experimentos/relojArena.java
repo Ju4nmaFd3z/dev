f374cb80b902027a8a00b8367c5f74fb11329d6d
Juanma Fernández <juanmafr2007@gmail.com>
2025-11-17 20:37:03 +0100

    relojArena

---
+++ src/experimentos/relojArena.java
@@ -0,0 +1,52 @@
+package experimentos;
+
+public class relojArena {
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    public static void main(String[] args) throws Exception {
+        int alturaMitad = 10;
+        int fila;
+        int nivelArenaArriba = 10;
+        int nivelArenaAbajo = 10;
+        do {
+            fila = alturaMitad;
+            while (fila > 1) {
+                for (int i = 1; i <= alturaMitad - fila; i++) {
+                    System.out.print(" ");
+                }
+                for (int i = 1; i <= 2 * fila + 1; i++) {
+                    if (i == 1 || fila == alturaMitad || i == 2 * fila + 1) 
+                        System.out.print("*");
+                    else if (nivelArenaArriba >= fila) 
+                        System.out.print("·");
+                    else 
+                        System.out.print(" ");
+                }
+                System.out.println();
+                fila--;
+            }
+            fila = 1;
+            while (fila <= alturaMitad) {
+                for (int i = 1; i <= alturaMitad - fila; i++) {
+                    System.out.print(" ");
+                }
+                for (int i = 1; i <= 2 * fila + 1; i++) {
+                    if (i == 1 || fila == alturaMitad || i == 2 * fila + 1) 
+                        System.out.print("*");
+                    else if (nivelArenaAbajo <= fila) 
+                        System.out.print("·");
+                    else 
+                        System.out.print(" ");
+                }
+                System.out.println();
+                fila++;
+            }
+            nivelArenaArriba--;
+            nivelArenaAbajo--;
+            Thread.sleep(500);
+            limpiarPantalla();
+        } while (true);
+    }
+}

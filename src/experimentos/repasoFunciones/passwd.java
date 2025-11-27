fcc9076532f8382990b7f3c8d4545c1786c5b33d
Juan Manuel Fernández Rodríguez <juanmafr2007@gmail.com>
2025-11-27 09:52:58 +0100

    Terminado: passwd

--- src/experimentos/repasoFunciones/passwd.java
+++ src/experimentos/repasoFunciones/passwd.java
@@ -1,5 +1,23 @@
 package experimentos.repasoFunciones;
+import experimentos.repasoFunciones.funciones.passwdUtility;
 
 public class passwd {
-    
+    // =========================
+    //   Pequeño main de prueba
+    // =========================
+    public static void main(String[] args) {
+        test("Hola123");
+        test("Hola123!");
+        test("holaholahola");
+        test("  A1!bcD  ");
+        test("12345678");
+        test("!!!!AAAA");
+    }
+    private static void test(String password) {
+        System.out.println("Password original: \"" + password + "\"");
+        System.out.println("Sanitizada:       \"" +  passwdUtility.sanitizarPassword(password) + "\"");
+        System.out.println("Tipos de chars:   " +  passwdUtility.contarTiposCaracteres( passwdUtility.sanitizarPassword(password)));
+        System.out.println("Clasificación:    " +  passwdUtility.clasificarPassword(password));
+        System.out.println("-------------------------------------");
+    }
 }
